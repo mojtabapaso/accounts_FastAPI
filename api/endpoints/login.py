cf7827df1bda3b8b4ea7008420c4d70921ea43b2
Mojtaba <mojtabapaso@gmail.com>
2023-07-18 14:58:38 +0330

    add doct in api and fix bug in schema

--- api/endpoints/login.py
+++ api/endpoints/login.py
@@ -5,7 +5,6 @@ from ..dependency.auth import validate_phone_number, user_exist, avoid_creating_
 from utils.sender import send_otp_code
 from utils.random import random_otp_code
 from db.dependencies import get_db
-from starlette import status
 from schemas import schema
 from ..dependency.jwt import AuthJWT
 from models.otpcode import OtpCode
@@ -19,7 +18,12 @@ def rt():
 
 
 @router.post('/phone/', dependencies=[Depends(user_exist), Depends(avoid_creating_additional_code)])
-def login_token(data: schema.OtpCode, db: Session = Depends(get_db)):
+def validate_phone_number_send_code(data: schema.OtpCode, db: Session = Depends(get_db)):
+    """
+    Validate the  **phone number** \n
+    validate user exist in database \n
+    If approved, the **confirmation code** will be created and sent
+    """
     otpCode = OtpCode(code=random_otp_code(), phone_number=data.phone_number)
     db.add(otpCode)
     db.commit()
@@ -27,15 +31,24 @@ def login_token(data: schema.OtpCode, db: Session = Depends(get_db)):
     return "send opt code"
 
 
-@router.post('/code/', dependencies=[Depends(user_exist), Depends(code_is_expired)])
-def login_token(data: schema.UserData, Authorize: AuthJWT = Depends()):
+@router.post('/code/', dependencies=[Depends(user_exist), Depends(code_is_expired)], response_model=schema.Token)
+def login_with_phone_number_and_code(data: schema.UserData, Authorize: AuthJWT = Depends()):
+    """
+    validate code has not expired \n
+    validate user exist in database \n
+    if otp code is not expired login user
+    """
     access_token = Authorize.create_access_token(subject=data.phone_number)
     refresh_token = Authorize.create_refresh_token(subject=data.phone_number)
     return {"access_token": access_token, "refresh_token": refresh_token}
 
 
-@router.post("/password/", dependencies=[Depends(validate_phone_number)])
-def login_password(data: str = Depends(login_password), Authorize: AuthJWT = Depends()):
+@router.post("/password/", dependencies=[Depends(validate_phone_number)], response_model=schema.Token)
+def login_with_password(data: str = Depends(login_password), Authorize: AuthJWT = Depends()):
+    """
+    validate user exist in database \n
+    if password is current login user
+    """
     refresh_token = Authorize.create_refresh_token(subject=data)
     access_token = Authorize.create_access_token(subject=data)
     return {"access_token": access_token, 'refresh_token': refresh_token}
--- schemas/schema.py
+++ schemas/schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Union
 
 
 class OtpCode(BaseModel):
@@ -6,33 +7,31 @@ class OtpCode(BaseModel):
 
 
 class UserBase(BaseModel):
-    phone_number: str = Field(examples="09123456789")
-
+    phone_number: str = Field(example="09331264063")
 
 
 class UserCreate(BaseModel):
     password: str
 
 
-class User(UserBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        orm_mode = True
+# class User(UserBase):
+#     id: int
+#     is_active: bool
+#
+#     class Config:
+#         orm_mode = True
 
 
 class UserData(UserBase):
-    code: int
+    code: str = Field(example="123456")
 
 
 class Token(BaseModel):
-    access_token: str
-    token_type: str
+    token: dict
 
-
-class UserInDB(User):
-    hashed_password: str
+#
+# class UserInDB(User):
+#     hashed_password: str
 
 
 class Password(BaseModel):
@@ -44,9 +43,9 @@ class PasswordUpdate(Password):
 
 
 class Profile(BaseModel):
-    email: str | None = None
-    first_name: str | None = None
-    last_name: str | None = None
+    email: Union[str, None] = Field(default=None, example="email@gmail.com")
+    first_name: Union[str, None] = Field(example="name")
+    last_name: Union[str, None] = Field(example="last name")
 
 
 class LoginPassword(Password, UserBase):
