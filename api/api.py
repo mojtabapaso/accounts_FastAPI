10a12c20cba26b4e633ea12ac9faff16dade3be3
Mojtaba <mojtabapaso@gmail.com>
2023-07-20 18:34:02 +0330

    add docs in openapi

--- api/api.py
+++ api/api.py
@@ -1,9 +1,46 @@
 from .endpoints import profile, register, login, password
 from fastapi import FastAPI
 
-app = FastAPI()
+tags_metadata = [
 
-app.include_router(profile.router, tags=["Profile"])
+    {
+        "name": "Register",
+        "description": "User Registration is done here",
+    },
+    {
+        "name": "Login",
+        "description": "User Login is done here",
+    },
+    {
+        "name": "Profile",
+        "description": "User Profile is done here",
+    },
+    {
+        "name": "Password",
+        "description": "Change and Set Password User here",
+    },
+]
+description = """
+# Simple Manager Account with FastAPI . 🚀
+# Attribute :
+## Register
+## Login
+## Profile
+## Password
+"""
+app = FastAPI(openapi_tags=tags_metadata, title="Simple FastAPI 🚀 Project",
+              description=description,
+              version="0.0.1",
+              contact={
+                  "name": "to Mojtaba",
+                  "email": "mojtabapaso@gmail.com",
+              },
+              license_info={
+                  "name": "License MIT",
+                  "identifier": "MIT",
+              }, )
+
 app.include_router(register.router, tags=["Register"])
 app.include_router(login.router, tags=["Login"])
+app.include_router(profile.router, tags=["Profile"])
 app.include_router(password.router, tags=["Password"])
